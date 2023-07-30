f2f1ca16e733278b2a56ba62512a1b0ee5f6b95d
unknown <franzgian.sg@gmail.com>
2023-07-30 20:16:44 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
